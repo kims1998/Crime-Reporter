0f5e14d247dc7e8fe69416160fbf225c9a31b719
SailmanSeeulater <latersailor@gmail.com>
2025-10-15 15:46:56 -0700

    -Map.jsx  - Added SDSUBounds, which bounds the map to only the SDSU area. - Deleted controller.java

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -3,6 +3,12 @@ const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 const SDSUMap = () => {
     const position = { lat: 32.7764, lng: -117.0719 };
+    const SDSUBounds = {
+        north: 32.788,
+        south: 32.765,
+        west: -117.095,
+        east: -117.050,
+    }
     return (
         <APIProvider apiKey={apiKey}>
             <Map
@@ -10,6 +16,10 @@ const SDSUMap = () => {
                 defaultZoom={ 16 }
                 style={{ width: '100%', height: '100%' }}
                 options={{
+                    restriction: {
+                        latLngBounds: SDSUBounds,
+                        strictBounds: true,
+                    },
                     zoomControl: true,
                     mapTypeControl: true,
                     streetViewControl: true,
